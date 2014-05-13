ceb2a101ccd5920a9d63317d9d4d16fbdcdcb89c
dimitraNat <dimitraNat@dimitra>
2014-05-13 20:45:04 +0300

    Add classes Player and Ship

---
+++ src/Ship.java
@@ -0,0 +1,18 @@
+import java.util.Set;
+
+public class Ship {
+	
+	private Set<UprageableShipAttribute> UprageableShipAttribute ;
+	private Set<InBattleShipAttribute> InBattleShipAttribute ;
+	private Set<StandardShipAttribute> StandardShipAttribute ;
+	
+	
+	public void applyUpgrade(Upgrade upgrade){
+		
+	}
+	
+	public void engageBattle(){
+		
+	}
+
+}
