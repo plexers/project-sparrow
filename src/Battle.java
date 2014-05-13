31dccff6119e74445ca463227e6a0eb4d33f065c
telis93 <dossas07@hotmail.com>
2014-05-14 00:00:41 +0300

    Add class Battle

---
+++ src/Battle.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+
+public class Battle {
+	private Player player1, player2;
+	private History history;
+	
+	public void submitTurnAndWaitForOpponnent(List<Action> turnActions) {
+		
+	}
+	public void applyResult() {
+		
+	}
+}
