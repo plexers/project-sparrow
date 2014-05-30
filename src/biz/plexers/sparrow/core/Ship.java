d9c5f05494904da5856f4a6656ecd43b235526e0
telis93 <dossas07@hotmail.com>
2014-05-31 00:33:59 +0300

    Change Ship serialization method

--- src/biz/plexers/sparrow/core/Ship.java
+++ src/biz/plexers/sparrow/core/Ship.java
@@ -1,10 +1,20 @@
 package biz.plexers.sparrow.core;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 import org.ektorp.support.CouchDbDocument;
 
-public class Ship extends CouchDbDocument{
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(using = Ship.Serializer.class)
+public class Ship extends CouchDbDocument {
 
 	private Set<UpgradableShipAttribute> upgradableShipAttributes;
 	private Set<InBattleShipAttribute> inBattleShipAttributes;
@@ -16,12 +26,6 @@ public class Ship extends CouchDbDocument{
 		return name;
 	}
 
-	public Ship(double goldValue, String name) {
-		super();
-		this.goldValue = goldValue;
-		this.name = name;
-	}
-
 	public void applyUpgrade(Upgrade upgrade) {
 
 	}
@@ -31,6 +35,46 @@ public class Ship extends CouchDbDocument{
 	}
 
 	public void engageBattle() {
+
+	}
+
+	private Ship(Map<String, Object> props) {
+		super();
+		goldValue = (double) props.get("goldValue");
+		name = (String) props.get("name");
+		// TODO Uncomment these lines where deserializers ready
+		// upgradableShipAttributes = (Set<UpgradableShipAttribute>)
+		// props.get("upgradableShipAttributes");
+		// inBattleShipAttributes = (Set<InBattleShipAttribute>)
+		// props.get("inBattleShipAttributes");
+		// standardShipAttributes = (Set<StandardShipAttribute>)
+		// props.get("standardShipAttributes");
+	}
+
+	@JsonCreator
+	public static Ship factory(Map<String, Object> props) {
+		return new Ship(props);
+	}
+
+	public class Serializer extends JsonSerializer<Ship> {
+
+		@Override
+		public void serialize(Ship value, JsonGenerator jgen,
+				SerializerProvider provider) throws IOException,
+				JsonProcessingException {
+			jgen.writeStartObject();
+			jgen.writeNumberField("goldVaule", value.goldValue);
+			jgen.writeStringField("name", value.name);
+			// TODO Uncomment these lines where serializers ready
+			// jgen.writeObjectField("inBattleShipAttributes",
+			// value.inBattleShipAttributes);
+			// jgen.writeObjectField("upgradableShipAttributes",
+			// value.upgradableShipAttributes);
+			// jgen.writeObjectField("standardShipAttributes",
+			// value.standardShipAttributes);
+			jgen.writeEndObject();
+
+		}
 
 	}
 
