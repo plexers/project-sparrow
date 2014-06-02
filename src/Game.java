f1dde25d80d0a323af6ef2c699bc2ea333477d31
mavrophor <petmav@gmail.com>
2014-06-02 13:34:11 +0300

    Update multiplayer method Add joinMpBattle, createMpBattle methods

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -7,6 +8,7 @@ import biz.plexers.sparrow.core.Ship;
 import biz.plexers.sparrow.core.UserManager;
 import biz.plexers.sparrow.db.exceptions.SignInException;
 import biz.plexers.sparrow.db.exceptions.SignUpException;
+import biz.plexers.sparrow.mp.BattleManager;
 import biz.plexers.sparrow.sp.ShipMarket;
 
 public class Game {
@@ -90,8 +92,7 @@ public class Game {
 	}
 
 	private static void singlePlayer() {
-		if (UserManager.shipExists())
-		{
+		if (UserManager.shipExists()) {
 			ShipMarket shipMarket = ShipMarket.getInstance();
 			List<Ship> ships = shipMarket.getShips();
 			for (int i = 0; i < ships.size(); i++) {
@@ -111,6 +112,45 @@ public class Game {
 	}
 
 	private static void multiPlayer() {
+		if (UserManager.shipExists()) {
+
+			System.out.print("1. Create Battle 2. Join Battle :");
+			int choice = Integer.parseInt(s.next());
+
+			switch (choice) {
+			case 1:
+				createMpBattle();
+				break;
+			case 2:
+				joinMpBattle();
+				break;
+			default:
+				System.out.println("Wrong choice");
+				break;
+			}
+
+		} else {
+			System.out.println("You have to get a ship in single player.");
+		}
+
+	}
+
+	private static void joinMpBattle() {
+		ArrayList<String> openBattles = (ArrayList<String>) BattleManager
+				.getBattleList();
+		System.out.print("Available Battles :");
+
+		for (int i = 0; i < openBattles.size(); i++) {
+			System.out.println(i + ": " + openBattles.get(i));
+		}
+		System.out.print("Choose Battle :");
+		int choice = Integer.parseInt(s.next());
+		
+		// TODO: couchdb join battle #choice
+
+	}
+
+	private static void createMpBattle() {
 		// TODO Auto-generated method stub
 
 	}
